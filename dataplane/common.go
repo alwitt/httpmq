a1fcfbea4b2deff4bd7257e2cf28d4fd6492341f
Rhine Cliff <rhinecliff@protonmail.com>
2021-12-06 22:05:40 -0800

    Create helper function to convert a nats.Msg for jetstream into a structure for delivery.

--- dataplane/common.go
+++ dataplane/common.go
@@ -1,6 +1,7 @@
 package dataplane
 
 import (
+	"encoding/base64"
 	"fmt"
 
 	"github.com/nats-io/nats.go"
@@ -19,4 +20,43 @@ func msgToString(msg *nats.Msg) string {
 		return msgName
 	}
 	return msg.Subject
+}
+
+// ==============================================================================
+
+// MsgToDeliverSeq JetStream message sequence parameter
+type MsgToDeliverSeq struct {
+	Stream   uint64 `json:"for_stream" validate:"gte=0"`
+	Consumer uint64 `json:"for_consumer" validate:"gte=0"`
+}
+
+// MsgToDeliver a structure for representing a message to send out
+type MsgToDeliver struct {
+	Stream   string          `json:"stream" validate:"required"`
+	Subject  string          `json:"subject" validate:"required"`
+	Consumer string          `json:"consumer" validate:"required"`
+	Sequence MsgToDeliverSeq `json:"sequence" validate:"required,dive"`
+	Message  string          `json:"msg_base64" validate:"required"`
+}
+
+// ConvertJSMessageDeliver convert a JetStream message for delivery
+func ConvertJSMessageDeliver(subject string, msg *nats.Msg) (*MsgToDeliver, error) {
+	meta, err := msg.Metadata()
+	if err == nil {
+		return &MsgToDeliver{
+			Stream:   meta.Stream,
+			Subject:  subject,
+			Consumer: meta.Consumer,
+			Sequence: MsgToDeliverSeq{
+				Stream: meta.Sequence.Stream, Consumer: meta.Sequence.Consumer,
+			},
+			Message: base64.StdEncoding.EncodeToString(msg.Data),
+		}, nil
+	}
+	return nil, err
+}
+
+// GetOriginalMsg given a message, get the original decoded data
+func (m *MsgToDeliver) GetOriginalMsg() ([]byte, error) {
+	return base64.StdEncoding.DecodeString(m.Message)
 }
--- dataplane/message_transport_test.go
+++ dataplane/message_transport_test.go
@@ -356,3 +356,125 @@ func TestMessageTransportPushSubGroup(t *testing.T) {
 	}
 	assert.Len(rxID, 2)
 }
+
+func TestMessageTranscoding(t *testing.T) {
+	assert := assert.New(t)
+	log.SetLevel(log.DebugLevel)
+	testName := "ut-js-msg-transcode"
+
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+	utCtxt, utCtxtCancel := context.WithCancel(context.Background())
+	defer utCtxtCancel()
+
+	logTags := log.Fields{
+		"module":    "dataplane_test",
+		"component": "JetStreamPushSubscriber",
+		"instance":  "message_transcode",
+	}
+
+	// Define NATS connection params
+	natsParam := core.NATSConnectParams{
+		ServerURI:           "nats://127.0.0.1:4222",
+		ConnectTimeout:      time.Second,
+		MaxReconnectAttempt: 0,
+		ReconnectWait:       time.Second,
+		OnDisconnectCallback: func(_ *nats.Conn, e error) {
+			if e != nil {
+				log.WithError(e).WithFields(logTags).Error(
+					"Disconnect callback triggered with failure",
+				)
+			}
+		},
+		OnReconnectCallback: func(_ *nats.Conn) {
+			log.WithFields(logTags).Debug("Reconnected with NATs server")
+		},
+		OnCloseCallback: func(_ *nats.Conn) {
+			log.WithFields(logTags).Debug("Disconnected from NATs server")
+		},
+	}
+
+	js, err := core.GetJetStream(natsParam)
+	assert.Nil(err)
+	defer js.Close(utCtxt)
+
+	jsCtrl, err := management.GetJetStreamController(js, testName)
+	assert.Nil(err)
+
+	// Define stream and consumers for testing
+	stream1 := uuid.New().String()
+	base := uuid.New().String()
+	subject1 := fmt.Sprintf("%s.primary", base)
+	{
+		maxAge := time.Second * 10
+		streamParam := management.JSStreamParam{
+			Name:     stream1,
+			Subjects: []string{subject1},
+			JSStreamLimits: management.JSStreamLimits{
+				MaxAge: &maxAge,
+			},
+		}
+		assert.Nil(jsCtrl.CreateStream(streamParam))
+	}
+	consumer1 := uuid.New().String()
+	{
+		param := management.JetStreamConsumerParam{
+			Name: consumer1, MaxInflight: 1, Mode: "push", FilterSubject: &subject1,
+		}
+		assert.Nil(jsCtrl.CreateConsumerForStream(stream1, param))
+	}
+	log.Debug("============================= 1 =============================")
+
+	// Case 0: define new subscribers
+	rxSub1, err := getJetStreamPushSubscriber(js, stream1, subject1, consumer1, nil)
+	assert.Nil(err)
+	log.Debug("============================= 2 =============================")
+
+	internalErrorHandler := func(err error) {
+		assert.Equal("nats: connection closed", err.Error())
+	}
+
+	// Case 1: start reading messages
+	rxChan1 := make(chan *nats.Msg, 1)
+	msgHandler1 := func(msg *nats.Msg, _ context.Context) error {
+		rxChan1 <- msg
+		return nil
+	}
+	assert.Nil(rxSub1.StartReading(msgHandler1, internalErrorHandler, &wg, utCtxt))
+	log.Debug("============================= 3 =============================")
+
+	publisher, err := GetJetStreamPublisher(js, testName)
+	assert.Nil(err)
+
+	// Case 2: publish a message
+	msg2 := []byte(uuid.New().String())
+	var encoded2 *MsgToDeliver
+	{
+		ctxt, cancel := context.WithTimeout(utCtxt, time.Second)
+		defer cancel()
+		assert.Nil(publisher.Publish(subject1, msg2, ctxt))
+	}
+	// verify receive
+	{
+		ctxt, cancel := context.WithTimeout(utCtxt, time.Second)
+		defer cancel()
+		select {
+		case <-ctxt.Done():
+			assert.False(true)
+		case msg, ok := <-rxChan1:
+			assert.True(ok)
+			assert.EqualValues(msg2, msg.Data)
+			assert.Nil(msg.AckSync())
+			enc, err := ConvertJSMessageDeliver(subject1, msg)
+			assert.Nil(err)
+			encoded2 = enc
+		}
+	}
+
+	// Case 3: verify the decoded message matches
+	{
+		decode, err := encoded2.GetOriginalMsg()
+		assert.Nil(err)
+		assert.EqualValues(msg2, decode)
+	}
+}
